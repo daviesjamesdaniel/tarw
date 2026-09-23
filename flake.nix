04a5ec9eaed181a3365b5121c73016c3c247ff81
Dan Davies <davies.james.daniel@gmail.com>
2026-09-23 11:17:59 +0100

    Bump version to 0.4.2

--- flake.nix
+++ flake.nix
@@ -70,7 +70,7 @@
         # 2026-09-14.
         sourceRoot = "source/src-tauri";
         pname = "tarw";
-        version = "0.4.1";
+        version = "0.4.2";
       };
 
       # Real caching restored, 2026-09-14 (see project memory): confirmed via
